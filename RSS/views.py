418ca29c7fd2d751aad425654921268b2d916a9c
Jurica <jurica.martinez@gmail.com>
2019-03-19 18:28:00 +0100

    serializers 1.2

--- RSS/views.py
+++ RSS/views.py
@@ -3,4 +3,10 @@ from .models import RSSfeed
 from .serializers import RSSfeedSerializer
 from rest_framework import generics
 
-class
+class ChannelList(generics.ListCreateAPIView): 
+	queryset = RSSfeed.objects.get(category="Sport", "Show")  #nije dobro
+	serializer_class = RSSfeedSerializer
+
+class AllFeeds(generics.ListCreateAPIView):
+	queryset = RSSfeed.objects.all()
+	serializer_class = RSSfeedSerializer
