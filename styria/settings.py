32e8a4a1bee0b3c967240b26f8068d6458f70b4a
Jurica <jurica.martinez@gmail.com>
2019-03-20 12:33:35 +0100

    povezivanje s Frontom
    Webpackage i babel

--- styria/settings.py
+++ styria/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'RSS.apps.RssConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
