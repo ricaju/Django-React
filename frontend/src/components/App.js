74f9083c07af5f1ac9a9dd7e853e7917164fc81a
Jurica <jurica.martinez@gmail.com>
2019-03-21 12:18:29 +0100

    webpack, babel?

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,11 +4,12 @@ import ReactDOM from "react-dom";
 
 class App extends Component {
 	render(){
-		console.log("blblbl")
-		return <h1>TESSSST1111</h1>
+		console.log("blblbl88888")
+		return (
+			<h1>TESSSST hghghghdva</h1>
+		)
 	}
 }
 
 
-
-ReactDOM.render(<App/>, document.getElementById("app"));
+export default App;
