65442f8204039de384052fa35fa9106e6439ca31
Jurica <jurica.martinez@gmail.com>
2019-03-24 12:48:31 +0100

    fixing bugs

--- Backend/RSS/models.py
+++ Backend/RSS/models.py
@@ -1,12 +1,4 @@
 from django.db import models
-import requests
-from bs4 import BeautifulSoup
-import html.parser
-
-news = requests.get('https://www.24sata.hr/feeds/news.xml')
-sport = requests.get('https://www.24sata.hr/feeds/sport.xml')
-show = requests.get('https://www.24sata.hr/feeds/show.xml')
-tech = requests.get('https://www.24sata.hr/feeds/tech.xml')
 
 
 class RSSfeed(models.Model):
@@ -18,66 +10,3 @@ class RSSfeed(models.Model):
 	pubDate = models.DateTimeField(blank=True, null=True, auto_now_add = True)
 	img = models.CharField(max_length=400)
 
-soupSport = BeautifulSoup(sport.content, features="xml")
-items = soupSport.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text)
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string))
-    img = i.img['src']
-    a = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    a.save()
-
-soupNews = BeautifulSoup(news.content, features="xml")
-items = soupNews.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text)
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string))
-    img = i.img['src']
-    b = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    b.save()
-
-soupShow = BeautifulSoup(show.content, features="xml")
-items = soupShow.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text)
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string))
-    img = i.img['src']
-    c = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    c.save()
-
-soupTech = BeautifulSoup(tech.content, features="xml")
-items = soupTech.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text)
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string))
-    img = i.img['src']
-    d = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    d.save()
