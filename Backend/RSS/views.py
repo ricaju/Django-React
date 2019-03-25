b631477a96b7544304349627637dcb2038da9d27
Jurica <jurica.martinez@gmail.com>
2019-03-25 11:16:49 +0100

    Update views.py

--- Backend/RSS/views.py
+++ Backend/RSS/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.db.utils import OperationalError
+from django.db.models import Q
 from .models import RSSfeed
 from .serializers import RSSfeedSerializer
 from rest_framework import generics
-from django.db.models import Q
 import requests
 from bs4 import BeautifulSoup
 import html.parser
@@ -12,68 +13,72 @@ sport = requests.get('https://www.24sata.hr/feeds/sport.xml')
 show = requests.get('https://www.24sata.hr/feeds/show.xml')
 tech = requests.get('https://www.24sata.hr/feeds/tech.xml')
 
-soupSport = BeautifulSoup(sport.content, features="xml")
-items = soupSport.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text, features="lxml")
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string), features="xml")
-    img = i.img['src']
-    a = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    a.save()
+try:
+    soupSport = BeautifulSoup(sport.content, features="xml")
+    items = soupSport.findAll('item')
+    for item in items:
+        category = item.category.text
+        title = item.title.text
+        creator = item.creator.text
+        description_soup = BeautifulSoup(item.description.text, features="lxml")
+        description = description_soup.text
+        link = item.link.text
+        pubDate = item.pubDate.text
+        h = html.parser.HTMLParser()
+        i = BeautifulSoup(h.unescape(item.description.string), features="xml")
+        img = i.img['src']
+        a = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
+        a.save()
 
-soupNews = BeautifulSoup(news.content, features="xml")
-items = soupNews.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text, features="lxml")
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string), features="xml")
-    img = i.img['src']
-    b = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    b.save()
+    soupNews = BeautifulSoup(news.content, features="xml")
+    items = soupNews.findAll('item')
+    for item in items:
+        category = item.category.text
+        title = item.title.text
+        creator = item.creator.text
+        description_soup = BeautifulSoup(item.description.text, features="lxml")
+        description = description_soup.text
+        link = item.link.text
+        pubDate = item.pubDate.text
+        h = html.parser.HTMLParser()
+        i = BeautifulSoup(h.unescape(item.description.string), features="xml")
+        img = i.img['src']
+        b = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
+        b.save()
 
-soupShow = BeautifulSoup(show.content, features="xml")
-items = soupShow.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text, features="lxml")
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string), features="xml")
-    img = i.img['src']
-    c = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    c.save()
+    soupShow = BeautifulSoup(show.content, features="xml")
+    items = soupShow.findAll('item')
+    for item in items:
+        category = item.category.text
+        title = item.title.text
+        creator = item.creator.text
+        description_soup = BeautifulSoup(item.description.text, features="lxml")
+        link = item.link.text
+        pubDate = item.pubDate.text
+        h = html.parser.HTMLParser()
+        i = BeautifulSoup(h.unescape(item.description.string), features="xml")
+        img = i.img['src']
+        c = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
+        c.save()
 
-soupTech = BeautifulSoup(tech.content, features="xml")
-items = soupTech.findAll('item')
-for item in items:
-    category = item.category.text
-    title = item.title.text
-    creator = item.creator.text
-    description_soup = BeautifulSoup(item.description.text, features="lxml")
-    description = description_soup.text
-    link = item.link.text
-    pubDate = item.pubDate.text
-    h = html.parser.HTMLParser()
-    i = BeautifulSoup(h.unescape(item.description.string), features="xml")
-    img = i.img['src']
-    d = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
-    d.save()
+    soupTech = BeautifulSoup(tech.content, features="xml")
+    items = soupTech.findAll('item')
+    for item in items:
+        category = item.category.text
+        title = item.title.text
+        creator = item.creator.text
+        description_soup = BeautifulSoup(item.description.text, features="lxml")
+        description = description_soup.text
+        link = item.link.text
+        pubDate = item.pubDate.text
+        h = html.parser.HTMLParser()
+        i = BeautifulSoup(h.unescape(item.description.string), features="xml")
+        img = i.img['src']
+        d = RSSfeed.objects.create(category=category, title=title, creator=creator, description=description, link=link, pubDate=pubDate, img=img)
+        d.save()
+
+except OperationalError:    #da se ne sprema u bazu dok još nije migrirana
+    pass
 
 
 class ChanelAll(generics.ListCreateAPIView): #createAPIVies je za post i get, staviti samo get?
