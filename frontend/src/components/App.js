88ae1f832ceb9d577ab7a5fd9862a01bf67164aa
Jurica <jurica.martinez@gmail.com>
2019-03-28 14:39:08 +0100

    css-loader

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 class App extends Component {
-	render(){
-		console.log("blblbl88888")
-		return (
-			<h1>radi </h1>
-		)
-	}
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/Home" exact={true} component={Home}/>
+        <Redirect to="/Home"/>                {/*dodati ostale routere za history.push?*/}
+        </Switch>
+      </Router>
+    );
+  }
 }
-
 
 export default App;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,26 +1,81 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import 'antd/dist/antd.css';
 import { withRouter } from 'react-router-dom';
+{/*import AllFeedsView from '../containers/AllFeedsView';
+import NewsView from '../containers/NewsView';
+import SportView from '../containers/SportView';
+import ShowView from '../containers/ShowView';
+import TechView from '../containers/TechView';
+import Search from '../containers/Search';*/}
 
-class Home extends Component {
+const { Header, Content, Footer } = Layout;
+
+
+class Home extends Component{
 	constructor(props){
 		super(props);
-		this.state = {
-			data: []
+		this.state ={
+			news: true,
+			all: false,
+			sport: false,
+			show: false,
+			tech: false
 		}
 	}
-
-	componentDidMount(){
-		fetch('api/AllFeeds')
-		.then(response => respnse.json())
-		.then(data => this.setState({ data: data}))
+	handelAll = () => {
+		this.setState({sport: false, show: false, news: false, tech: false, all: true})
+	}
+	handelNews = () => {
+		this.setState({sport: false, show: false, news: true, tech: false, all: false})
+	}
+	handelSport = () => {
+		this.setState({sport: true, show: false, news: false, tech: false, all: false})
+	}
+	handelShow = () => {
+		this.setState({sport: false, show: true, news: false, tech: false, all: false})
+	}
+	handelTech = () => {
+		this.setState({sport: false, show: false, news: false, tech: true, all: false})
 	}
 
-	render(){
-		console.log("blblbl")
-		<div>
-			tu smo
-		</div>
-	}
+ render(){
+ 	return(
+  <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1" onClick={this.handelNews}>News</Menu.Item>
+        <Menu.Item key="2" onClick={this.handelSport}>Sport</Menu.Item>
+        <Menu.Item key="3" onClick={this.handelShow}>Show</Menu.Item>
+        <Menu.Item key="4" onClick={this.handelTech}>Tech</Menu.Item>
+        <Menu.Item key="5" onClick={this.handelAll}>All Feeds</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Search/>
+      </Breadcrumb>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+      {/*}	{this.state.news ? <NewsView/> : null}
+      	{this.state.sport ? <SportView/> : null}
+      	{this.state.show ? <ShowView/> : null}
+      	{this.state.tech ? <TechView/> : null}
+      	{this.state.all ? <AllFeedsView/> : null} */}
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      24 FEEDS
+    </Footer>
+  </Layout>
+  )
 }
 
-export default Home;
+}
+
+export default withRouter(Home);
